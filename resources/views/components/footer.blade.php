0b3389f156b14e63ac61b66efa263901458e206f
Adam PM <adam.pm77@gmail.com>
2025-02-06 23:17:22 +0700

    perbaikan footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="flex-shrink-0 px-6 py-4">
-    <p class="dark:text-gray-400 flex items-center justify-center gap-1 text-sm text-gray-600">
-        <span>Copyright &copy; {{ date('Y') }}</span>
+<footer class="px-6 py-4">
+    <p class="dark:text-gray-400 flex items-center justify-center gap-1 text-xs text-gray-600">
+        <span class="md:block hidden text-xs">Copyright &copy; {{ date('Y') }}</span>
 
-        <span>by</span>
+        <span class="md:block hidden text-xs">by</span>
 
-        <a href="https://sistegra.id" target="_blank" class="hover:underline text-blue-600">
+        <a href="https://sistegra.id" target="_blank" class="hover:underline md:block hidden text-xs text-blue-600">
             Sistegra Emran Sentosa
         </a>
 
